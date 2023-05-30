c5506674d15e52662c48ce2856665ad1ac959a6d
Aleenabrink93 <aleenabrink@gmail.com>
2023-05-30 15:35:57 -0600

    removed dropdown list, set up local storage for cities search

--- assets/results.js
+++ assets/results.js
@@ -2,9 +2,9 @@
 var seatGeekURL = `https://api.seatgeek.com/2/venues?client_id=MzM4NjkxMjN8MTY4NDgwNzIxOS45Nzg3Mjgz&per_page=5`
 
 // Aleena: fetch when the page is loaded
-window.onload = function(lat,lon){
-
-fetch(seatGeekURL)
+window.onload = function(){
+var cities = localStorage.getItem("cities");
+fetch(seatGeekURL+"&city="+cities)
 .then(function(response){
     return response.json();
 })
